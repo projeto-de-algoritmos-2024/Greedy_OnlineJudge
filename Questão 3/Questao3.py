1ceecc0e5dbd468927ae25c3faf9eee47473bd63
João Barreto <108282056+JoaoBarreto03@users.noreply.github.com>
2024-07-28 15:27:33 -0300

    Versão final - Accepted

--- Questão 3/Questao3.py
+++ Questão 3/Questao3.py
@@ -5,10 +5,10 @@ def pylons(k, arr):
 
     while i < n:
         furthest = min(i + k - 1, n - 1)
-        while furthest > i - (k - 1) and arr[furthest] != 1:
+        while furthest >= max(0, i - k + 1) and arr[furthest] != 1:
             furthest -= 1
-        if furthest < i - (k - 1):
-            break
+        if furthest < max(0, i - k + 1):
+            return -1
         plants += 1
         i = furthest + k
     return plants
